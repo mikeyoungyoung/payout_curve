94494cb96147463f6b58aef1863df5a2c23834ed
mikeyoungyoung <mikeyoung.young@gmail.com>
2013-05-04 13:55:56 -0400

    Updating codebase

--- classes.rb
+++ classes.rb
@@ -24,8 +24,32 @@ class Curve
     #This calulates the payout % between two points
     def payout(a,b,c)
         if c.is_a? Float #Fixnum
-            #puts "Payout between points #{a} and #{b} at the value #{c} is..."
-            #payout =
+            puts "from inside"
+            x_array = @int_curve.map{ |x,y| x.to_f}
+            #puts x_array
+            #initialize loop
+            low_p = 0.0
+            high_p = 0.0
+            puts "checking array"
+            puts "#{c} --> #{low_p}"
+            #while high_p <= c
+                x_array.each_with_index do |element,index|
+                    puts "Is #{c} <= #{element}"
+                    #check if the evaluation point is in a range
+                    if c < element
+                        #Question:  How to make this stop the first time this happens?
+                        puts "C is less than #{element}"
+                        b = index
+                        puts "#{b}"
+                    elsif c > element
+                        puts "C is in the range"
+                        a = index - 1
+                        puts "#{a}"
+                    end
+                
+                end
+            #end
+            puts "Evaluate using: #{a} - #{b}"
             (@int_curve[b][1].to_f-@int_curve[a][1].to_f)/(@int_curve[b][0].to_f-@int_curve[a][0].to_f)*(c - @int_curve[a][0].to_f) + @int_curve[a][1].to_f
         else
             puts "This is not a number!"
@@ -35,12 +59,8 @@ class Curve
     
 end
 
-
-#profit = Curve.new("profit")
-#puts profit.name
-#profit.create_curve("curve_profit.txt")
-#puts "Calculate payout"
-#puts profit.int_curve[1][1]
-#puts profit.payout(2,3,25)
-#puts "testing"
-#puts profit.payout(2,3,"test")
+profit = Curve.new("profit")
+puts profit.name
+profit.create_curve("curve_profit.txt")
+puts "Check the Method"
+puts profit.payout(2.0,3.0,25.0)
--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-            $("#btn3").click(function(){
-                        $("#test3").val( // "Dolly Duck"
-                                        function(){
-                                        $("#curves option:selected").text();
-                                        });
-                             });
+            //$("#btn3").click(function(){
+            //            $("#test3").val( // "Dolly Duck"
+            //                            function(){
+            //                            $("#curves option:selected").text();
+            //                            });
+            //                 });
             $("select").change(function () {
                                      var str = "";
                                      $("select option:selected").each(function () {

0bd6d2ce068acb282494134dcdd4aa8d60a9baae
mikeyoungyoung <mikeyoung.young@gmail.com>
2013-09-13 22:45:46 -0400

    new design

--- app.rb
+++ app.rb
@@ -6,11 +6,6 @@ require 'json'
 require_relative 'classes'
 include FileUtils::Verbose
 
-#for global authentification
-#use Rack::Auth::Basic, "Restricted Area" do |username, password|
-#    username == 'admin' and password == 'admin'
-#end
-
 # We set the cache control for static resources to approximately 1 month
 set :static_cache_control, [:public, :max_age => 2678400]
 
@@ -19,32 +14,18 @@ use Rack::Deflater
 
 set :haml, :format => :html5
 
-#^^^^^^^^^^^^^^^^
-#Note:  Maybe here I can read a .txt files in a directory and create the curves
-
 #Create curve objects
 curves = Hash.new
 #for each file in curves director
 Dir.chdir("./public/curves") do
     files = Dir.glob("*.txt")
-    #puts "These files will be used:"
-    #puts files
-    #puts "^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
-    #create curves for each file
-    #i = 0
     files.each do |file|
-        #puts "Creating: #{file}"
         filename = file.to_s
         object = Curve.new(filename)
         object.create_curve(filename)
         curves[file] = object
-        #curves.each_key {|k| puts "#{k}: iteration #{i}" }
-        #i += 1
     end
 end
-#puts "List all curves"
-#curves.each_key {|k| puts curves[k].name}
-#puts Dir.pwd
 
 #class WebApp < Sinatra::Base
 
@@ -57,7 +38,7 @@ get '/' do
     @point = params[:message]
     @curves = curves
     @val_pts = Hash.new
-    #map to the view
+
     haml :index
 end
 
@@ -69,7 +50,7 @@ post '/' do
     @val_pts = Hash.new
     #check if evaluation point <=0, set to near zero if true
     @point = @point <= 0 ? 0.0001 : @point
-
+    #fit hash with evaluation result for each curve
     @curves.each_pair do |k,v|
         @val_pts[k] = v.payout(@point).round(2)
     end
@@ -91,7 +72,6 @@ get '/tiles' do
 
     @curves = curves
     @sym = @curves.keys[0]
-    #map to the view
 
     haml :tiles
 end
@@ -115,6 +95,7 @@ get '/admin' do
     haml :admin
 end
 
+#do I need this piece?
 get '/admin/upload' do
     #upload the file to the server location
     protected!
@@ -122,25 +103,6 @@ get '/admin/upload' do
 end
 
 post '/admin/upload' do
-    #unless params[:file] &&
-    #    (tmpfile = params[:file][:tempfile]) &&
-    #    (name = params[:file][:filename])
-    #    @error = "No file selected"
-    #    return haml(:admin)
-    #end
-    #STDERR.puts "Uploading file, original name #{name.inspect}"
-    #while blk = tmpfile.read(65536)
-    #    # here you would write it to its final location
-    #    directory = "./public/curves"
-    #    path = File.join(directory, name)
-    #    File.open(path, "wb") do |f|
-    #        f.write(tmpfile.read)
-    #        f.close
-    #    end
-    #
-    #    STDERR.puts blk.inspect
-    #end
-    #"Upload complete"
     
     if params[:file]
         filename = params[:file][:filename]
@@ -158,6 +120,23 @@ post '/admin/upload' do
     redirect '/admin'
 end
 
+delete '/admin/delete' do
+    
+    if params[:file]
+        #filename = params[:file][:filename]
+        filename = params[:file]
+
+        directory = "./public/curves"
+        puts filename
+        puts File.join(directory, filename)
+        #File.delete(File.join(directory, filename), 'wb')
+        flash 'Upload successful'
+        else
+        flash 'You have to choose a file'
+    end
+    
+    redirect '/admin'
+end
 
 
 __END__
--- classes.rb
+++ classes.rb
@@ -21,10 +21,6 @@ end
 
 hash_to_table = Proc.new { |k,v| "<tr><td>#{k}</td><td>#{v}</td></tr>"}
 
-#test_hash ={1=>1, 2=>2, 3=>3}
-#rows = test_hash.collect.each(&hash_to_table)
-#puts rows
-
 class Curve
     attr_accessor :data, :name, :filename, :int_data, :int_curve
     def initialize(name)
@@ -37,9 +33,6 @@ class Curve
     def create_curve(filename)
         @filename = filename
         @int_data = {}
-        #puts ">>>>> Inside create_curve method"
-        #puts ">>>>> #{@filename}"
-        
         File.open(@filename, 'r').each_line do |line|
             line = line.strip.split ' '
             @int_data[line.first.to_s] = line.last.to_s
@@ -59,13 +52,11 @@ class Curve
             
             #Reset the evaluation point if it breaches the upper limit of the curve
             c = c >= @x_arr.last.to_f ? @x_arr.last.to_f - $tol : c
-            
             #get low array and low point
             low_arr = @int_curve.select {|x,y| x.to_f < c }
             low_p = low_arr.last
             #get high array and high point
             high_arr = @int_curve.select {|x,y| x.to_f >= c }
-            #high_p = c >= high_arr.first ? high_arr.first[0] - eps : high_arr.first
             high_p = high_arr.first
             #Evaluate equation of line in two point form
             return (high_p[1].to_f-low_p[1].to_f)/(high_p[0].to_f-low_p[0].to_f)*(c - low_p[0].to_f) + low_p[1].to_f
